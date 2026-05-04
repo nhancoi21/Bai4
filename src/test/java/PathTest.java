ebfd753774eac9566f816b2d006215a79e92b535
nguyentrongnhan <25020301@vnu.edu.vn>
2026-05-04 22:41:25 +0700

    test

--- src/test/java/PathTest.java
+++ src/test/java/PathTest.java
@@ -1,15 +1,17 @@
+package banksystem;
+
 import org.junit.jupiter.api.Test;
-
 import java.io.File;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PathTest {
     @Test
-    public void testFilePathConfig() {
-        String path = "target\\test-classes\\test.txt";
-        File file = new File(path);
+    public void testFilePathFailure() {
+        String wrongPath = "src\\test\\java\\banksystem\\PathTest.java";
+        File file = new File(wrongPath);
 
-        assertEquals(true, file.exists());
+        System.out.println("Checking path on " + System.getProperty("os.name") + ": " + wrongPath);
+
+        assertTrue(file.exists(), "Lỗi: Không tìm thấy file trên hệ điều hành này!");
     }
 }
